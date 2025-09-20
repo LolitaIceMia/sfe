26902929bee5180b7807157de426009becb1ecaa
狐瓜西 <724095698ice@gmail.com>
2025-09-20 09:57:48 +0800

    first commit

---
+++ src/client/java/com/foxsoftware/client/SfeClient.java
@@ -0,0 +1,10 @@
+package com.foxsoftware.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SfeClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
