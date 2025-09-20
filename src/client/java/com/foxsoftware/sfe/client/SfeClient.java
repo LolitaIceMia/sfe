c6460a2fe32d22696889d036089398e7763b16bf
狐瓜西 <724095698ice@gmail.com>
2025-09-20 10:44:04 +0800

    Save

---
+++ src/client/java/com/foxsoftware/sfe/client/SfeClient.java
@@ -0,0 +1,10 @@
+package com.foxsoftware.sfe.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SfeClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/com/foxsoftware/sfe/Sfe.java
@@ -0,0 +1,10 @@
+package com.foxsoftware.sfe;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Sfe implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
